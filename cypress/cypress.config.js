a6aec61bc6c7bad66dba1c607c23aedfe1c44ca3
PaulaRosero30 <paulaandrea1595.rosero@gmail.com>
2026-05-11 18:26:09 -0500

    Eliminar archivos Cypress

--- cypress/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3002',
-    viewportWidth: 1280,
-    viewportHeight: 720,
-  },
-})
